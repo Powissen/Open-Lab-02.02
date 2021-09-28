be4505710ec51f0e4e8e2ff88d30572bfa2eb8ec
BuildTools <unconfigured@null.spigotmc.org>
2021-09-28 22:53:11 +0200

    Nefunkcne

--- Open-Lab-02.02/Formatter.cs
+++ Open-Lab-02.02/Formatter.cs
@@ -6,7 +6,10 @@ namespace Open_Lab_02._02
     {
         public void Format(double[] numbers)
         {
-            throw new NotImplementedException();
+            foreach (double num in numbers) 
+            { 
+                Console.Write(String.Format("{0:0.00} ", num)); 
+            }
         }
     }
 }
